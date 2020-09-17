a83506d334ea702da83d767c52f290d24d63109d
corleyb2 <corleyb2@gmail.com>
2020-09-17 15:16:35 -0400

    down & up auto-scrolling. behavior needs fine-tune

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Section from "./Components/Section";
 import dummyText from "./Components/DummyText";
+import { animateScroll as scroll } from "react-scroll";
 
 // This repo is a recreation of a tutorial on React-Scroll, using the newest React version and converting components from class to functional.  I'll be working to use more of the React-Scroll library.  S/o to James Quick, who authored the original tutorial and repo.
 
@@ -12,6 +13,29 @@ import dummyText from "./Components/DummyText";
 // Link to original public repo: https://github.com/do-community/React-With-Smooth-Scrolling
 
 function App() {
+  //not operational - but why?
+  // const autoScrollDown = () => {
+  //   scroll.scrollToBottom({
+  //     duration: 10000,
+  //     delay: 2000,
+  //     smooth: "linear",
+  //   });
+  // };
+
+  //also not operational
+  // const resetToTop = () => {
+  //   scroll.scrollToTop({
+  //     duration: 100,
+  //     delay: 2000,
+  //     smooth: false,
+  //   });
+  // };
+
+  // This works from Navbar, but not from App (when paired with a HTML button onClick) - what gives?
+  // const scrollToTop = () => {
+  //   scroll.scrollToTop();
+  // };
+
   return (
     <div className="App">
       <Navbar />
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../logo.svg";
 import { Link, animateScroll as scroll } from "react-scroll";
 
 function Navbar() {
-  const scrollToTop = () => {
-    scroll.scrollToTop();
+  //on refresh, state persists but window location remains... why?
+  const [viewLocation, setViewLocation] = useState({
+    head: true,
+    foot: false,
+  });
+
+  //Need to trigger a re-render somehow - this only works 1x
+  const handleScroll = async () => {
+    if (viewLocation.head) {
+      await scroll.scrollToBottom();
+      setViewLocation({ head: false, foot: true });
+    } else if (viewLocation.foot) {
+      await scroll.scrollToTop();
+      setViewLocation({ head: true, foot: false });
+    }
   };
+
+  console.log("view", viewLocation);
+
+  useEffect(() => {
+    setTimeout(() => handleScroll(), 4000);
+  }, [viewLocation]);
 
   return (
     <nav className="nav" id="navbar">
       <div className="nav-content">
-        <img src={logo} className="nav-logo" alt="Logo" onClick={scrollToTop} />
+        <img src={logo} className="nav-logo" alt="Logo" />
         <ul className="nav-items">
+          <li className="nav-item">
+          </li>
           <li className="nav-item">
             <Link
               activeClass="active"
